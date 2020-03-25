22d40c64af0c6834b6f0ec07a03fcd82e0e528be
evandrotvc <evandro-tvc@hotmail.com>
2020-03-25 20:02:56 -0300

    stripe payments included

--- app.js
+++ app.js
@@ -12,8 +12,16 @@ var session = require('express-session')
 var passport = require('passport')
 var flash = require('connect-flash')
 var validator = require('express-validator')
+var MongoStore = require('connect-mongo')(session)
+var bodyParser = require('body-parser')
 
 var app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 //mongoose.connect('localhost:27017/shopping')
 mongoose.connect('mongodb://localhost:27017/shopping' , 
@@ -32,7 +40,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(validator())
 app.use(cookieParser());
-app.use(session({secret: 'mysupersecret' , resave: false, saveUninitialized: false})) // chave segurança csurf
+app.use(session({
+  secret: 'mysupersecret' , 
+  resave: false, 
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  cookie: { maxAge: 180 * 60 * 1000}
+})) // chave segurança csurf
 app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session())
@@ -41,6 +55,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res , next){
     //req.isAuthenticated() will return true if user is logged in
     res.locals.login = req.isAuthenticated()
+    res.locals.session = req.session
     next()
 })
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var Cart = require('../models/cart')
 var Product = require('../models/products')
 
 /* GET home page. Docs guarda todos os dados do banco de dados */
 router.get('/', function(req, res, next) {
+   var sucessMsg = req.flash('sucess')[0]
    Product.find(function(err , docs){
     var productChunks = []
     var chunkSize = 3
@@ -12,11 +13,73 @@ router.get('/', function(req, res, next) {
     for(var i =0 ; i< produtos_size; i+= chunkSize){
       productChunks.push(docs.slice(i , i + chunkSize))
     }
-    res.render('shop/index', { title: 'Shopping Cart xD' , products: productChunks}); // productChunks é enviado para o layout da página
+    res.render('shop/index', { title: 'Shopping Cart xD' , products: productChunks , sucessMsg: sucessMsg , noMessages: !sucessMsg}); // productChunks é enviado para o layout da página
    }) 
   
 });
 
+router.get('/add-to-cart/:id' , function(req, res , next){
+  var productId = req.params.id
+  var cart = new Cart(req.session.cart ? req.session.cart : { })
 
+  Product.findById(productId , function(err, product){
+    if(err){
+      return res.redirect('/')
+    }
+    cart.add(product , product.id)
+    req.session.cart = cart
+    console.log(req.session.cart)
+    res.redirect('/')
+  })
+})
 
+router.get('/shoppingCart' , function(req, res , next){
+  if(!req.session.cart){
+    return res.render('shop/shoppingCart' , {products : null })
+  }
+  var cart = new Cart(req.session.cart)
+  res.render('shop/shoppingCart' , {products : cart.generateArray(), totalPrice: cart.totalPrice } )
+})
+
+router.get('/checkout' , function(req, res , next){
+  if(!req.session.cart){
+    return res.redirect('/shopping-cart')
+  }
+  console.log("request0:" , req.body)
+  var cart = new Cart(req.session.cart)
+  var errMsg = req.flash('error')[0]
+  res.render('shop/checkout' , {total : cart.totalPrice , errMsg : errMsg , noErrors : !errMsg})
+})
+
+// route card credits
+router.post('/checkout' , async (req, res, next) => {
+    if(!req.session.cart){
+      return res.redirect('/shopping-cart')
+    }
+    
+    var cart = new Cart(req.session.cart)
+    const stripe = require('stripe')('your_secret_key');
+    //const teste = req.body
+    //console.log("req:" , this.req)
+  // `source` is obtained with Stripe.js; see https://stripe.com/docs/payments/accept-a-payment-charges#web-create-token
+const charge=  await stripe.charges.create(
+    {
+      amount: cart.totalPrice * 100,
+      currency: 'usd',
+     source: req.body.stripeToken ,
+      description: 'Test Charge',
+    },
+    function(err, charge) {
+      // asynchronously called
+      if(err){
+        // error: must provide source or custom error
+        req.flash('error' , err.message )
+        return res.redirect('/checkout')
+      }
+      req.flash('sucess' , ' 🛍️ Sucessfully bought product!')
+      req.cart = null 
+      res.redirect('/')
+    }
+  );
+})
 module.exports = router;
